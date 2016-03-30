dd3310fbb63f1650e3ce65ee74312c34a7841aa4
nufail89 <nuvailmizzan@gmail.com>
2016-03-30 11:49:56 +0700

    Making Project
    Membuat project dari awal dan menambahkan libraries, langkahnya adalah
    1. Download
    http://dl.google.com/android/ndk/android-ndk-r10e-windows-x86_64.exe
    2. Extract file tersebut di C:\android-ndk-r10e
    3. Buka System Properties->Advanced system setting->Environment
    Variabels di pojok kanan bawah, kemudian tambahkan environmental
    variables:
    
    JAVA_HOME -> C:\Program Files\Java\jdk1.8.0_45
    
    note: sesuaikan versi jdk-nya dengan jdk Anda
    
    PATH ->
    C:\android-ndk-r10e;C:\android_sdk\platform-tools;C:\android_sdk\tools
    
    4. Download dan extract sembarangan https://github.com/rmtheis/tess-two
    5. Buat folder libraries di folder utama project Anda.
    6. Cari folder tess-two di dalam tess-two-master yang udah di download
    di langkah 4 dan cut folder tersebut kemudian paste di folder libraries
    yg ada di project Anda.
    6. Klik kanan di start menu kemudian pilih CommandPrompt Admin, masuk ke
    folder libraries\tess-two dengan menggunakan perintah "CD
    PathProjectAnda\libraries\tesstwo" kemudian ketikkan perintah :
    NDK-Build

---
+++ app/src/main/java/com/nufail/simple_ocr/Utama.java
@@ -0,0 +1,12 @@
+package com.nufail.simple_ocr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Utama extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utama);
+    }
+}
